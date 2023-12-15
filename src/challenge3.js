827594f03adf17cabbba529150a93587f570e9fe
Elena <aa.elena@ymail.com>
2023-12-15 00:43:40 +0000

    Created challenge 4 file

--- src/challenge3.js
+++ src/challenge3.js
@@ -1,4 +1,5 @@
 // Declare a `function` called `greet` that has 1 argument called `name` and returns a String `"Hi, <name>!"`
+// This time use an arrow function
 
 function greet(name) {
     return `Hi, ${name}!`
